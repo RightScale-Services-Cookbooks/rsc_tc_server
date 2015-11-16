20f4a4da83f9de4466318f0717d835baaafaf987
gonzalez <edds718@gmail.com>
2015-11-16 14:49:48 -0500

    adding recipe

--- recipes/install_liferay.rb
+++ recipes/install_liferay.rb
@@ -16,8 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe 'liferay::default'
 
-liferay_app 'lifeapp' do
+liferay_stack_app 'lifeapp' do
   app_name 'helloworld'
 end
